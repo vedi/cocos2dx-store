66102f9d6dde4d0f08ad751f83504125d22ddca5
refaelos <refaeldakar@gmail.com>
2012-11-11 18:53:36 +0200

    docs

--- cocos2dx-store/ios/cocos2dx_StoreInfo.mm
+++ cocos2dx-store/ios/cocos2dx_StoreInfo.mm
@@ -1,10 +1,18 @@
-//
-//  cocos2dx_StoreInfo.cpp
-//  cocos2dx-store
-//
-//  Created by Refael Dakar on 10/28/12.
-//
-//
+/*
+ * Copyright (C) 2012 Soomla Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include "cocos2dx_StoreInfo.h"
 #import "VirtualCurrencyPack.h"
