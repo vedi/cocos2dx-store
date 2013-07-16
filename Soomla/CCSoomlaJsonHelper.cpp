b51d5233ad4cc5d901e97d95305aaf8b977d9b8f
Fedor Shubin <shubin.fedor@gmail.com>
2013-07-16 19:11:58 +0700

    Apply Boolean fix from EasyNDK: fixes #1604

--- Soomla/CCSoomlaJsonHelper.cpp
+++ Soomla/CCSoomlaJsonHelper.cpp
@@ -41,7 +41,7 @@ CCObject *CCSoomlaJsonHelper::getCCObjectFromJson(json_t *obj) {
         return array;
     }
     else if (json_is_boolean(obj)) {
-        CCBool *ccBool = CCBool::create((bool) json_boolean(obj));
+        CCBool *ccBool = CCBool::create(json_is_true(obj));
         return ccBool;
     }
     else if (json_is_integer(obj)) {
